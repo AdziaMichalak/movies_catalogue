32685e91dc9974fe6311942dc4ff28a38b890bba
Magdalena Michalak <mbawolik80@gmail.com>
2022-07-08 13:45:52 +0200

    10.4

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import tmdb_client
+import random
 
 app = Flask(__name__)
 
@@ -13,6 +14,17 @@ def utility_processor():
     def tmdb_poster_url(path, size):
         return tmdb_client.get_poster_urls(path, size)
     return {"tmdb_poster_url": tmdb_poster_url}
+
+#@app.route("/movie/<movie_id>")
+#def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    return render_template("movie_details.html", movie=details)
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+   details = tmdb_client.get_single_movie(movie_id)
+   cast = tmdb_client.get_single_movie_cast(movie_id)
+   return render_template("movie_details.html", movie=details, cast=cast)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tmdb_client.py
+++ tmdb_client.py
@@ -2,12 +2,14 @@ from flask import Flask
 import requests
 import random
 
+API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NzVjYzBlZWYxNTg4OGM4YTRjNjI1ZjcwMjc4ZTlmMCIsInN1YiI6IjYyYzU4NzJiZThkMDI4MDBjOWFiM2MxNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vyREpDimE4jZ_ib1fRxB08D0cQlZI6Z5bPaRAa2vxlU"
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'alamakota'
 
 def get_popular_movies():
     url = "https://api.themoviedb.org/3/movie/popular"
-    headers = {'Authorization': "Bearer %s" % "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4Y2FiOGMyMjEzZmM0MjFjYmQ1Yzk3ZDE4OGU2MmRlOCIsInN1YiI6IjYyYmM3Yjk1ZTFmYWVkMWRjNzc2OWMxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.gT60CG_IgbCzJG3PMjGMWEuc4eb86-7yIJaGhR-DEv4"}
+    headers = {'Authorization': "Bearer %s" % "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NzVjYzBlZWYxNTg4OGM4YTRjNjI1ZjcwMjc4ZTlmMCIsInN1YiI6IjYyYzU4NzJiZThkMDI4MDBjOWFiM2MxNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vyREpDimE4jZ_ib1fRxB08D0cQlZI6Z5bPaRAa2vxlU"}
     r =  requests.get(url, headers=headers)
     return r.json()
 
@@ -20,3 +22,27 @@ def get_movies(how_many):
     data = get_popular_movies()
     random.shuffle(data['results'])
     return data["results"][:how_many]
+
+def get_single_movie(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_single_movie_cast(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()["cast"]
+
+def get_movie_images(movie_id):
+    endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
+    headers = {
+        "Authorization": f"Bearer {API_TOKEN}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
