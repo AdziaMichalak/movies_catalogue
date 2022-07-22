26a25e01389886262d932f9542e594032540e721
Magdalena Michalak <mbawolik80@gmail.com>
2022-07-22 16:24:27 +0200

    update

--- main.py
+++ main.py
@@ -76,3 +76,4 @@ def show_favorites():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,4 +1,4 @@
-from app import app
+from main import app
 from unittest.mock import Mock
 import tmdb_client
 import pytest
